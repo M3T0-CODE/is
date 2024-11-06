384b61ad2011b7d734152a2519b5d943528e0c45
fares <faresahmed02811@gmail.com>
2024-11-06 19:44:24 +0200

    UCS Algorithm

--- Main.py
+++ Main.py
@@ -1,50 +1,60 @@
-class Graph:
-    def __init__(self):
-        self.graph = {}
-
-    def add_edge(self, u, v):
-        if u not in self.graph:
-            self.graph[u] = []
-        if v not in self.graph:
-            self.graph[v] = []
-        self.graph[u].append(v)
-        self.graph[v].append(u)
-
-    def dfs(self, start, visited=None):
-        if visited is None:
-            visited = set()
-        visited.add(start)
-        print(start, end=' ')
-
-        for neighbor in self.graph.get(start, []):
-            if neighbor not in visited:
-                self.dfs(neighbor, visited)
-
-    def bfs(self, start):
-        visited = set()
-        queue = [start]
-        visited.add(start)
-
-        while queue:
-            node = queue.pop(0)
-            print(node, end=' ')
-
-            for neighbor in self.graph.get(node, []):
-                if neighbor not in visited:
-                    visited.add(neighbor)
-                    queue.append(neighbor)
+import heapq
 
 
-if __name__ == "__main__":
-    g = Graph()
-    g.add_edge(0, 1)
-    g.add_edge(0, 2)
-    g.add_edge(1, 2)
-    g.add_edge(1, 3)
-    g.add_edge(2, 4)
-    g.add_edge(3, 4)
+def uniform_cost_search(goal, start):
+    global graph, cost
+    answer = [float('inf')] * len(goal)
+    queue = [(0, start)]
+    visited = set()
+    count = 0
 
-    print("Depth-First Search (DFS):")
-    g.dfs(0)
-    print("\nBreadth-First Search (BFS):")
-    g.bfs(0)
+    while queue:
+        queue.sort()
+        current_cost, current_node = queue.pop(0)
+
+        if current_node in goal:
+            index = goal.index(current_node)
+            if answer[index] == float('inf'):
+                count += 1
+            answer[index] = min(answer[index], current_cost)
+
+            if count == len(goal):
+                return answer
+
+        if current_node not in visited:
+            visited.add(current_node)
+            for neighbor in graph[current_node]:
+                new_cost = current_cost + cost.get((current_node, neighbor), float('inf'))
+                heapq.heappush(queue, (new_cost, neighbor))
+
+    return answer
+
+
+if __name__ == '__main__':
+    graph = [[] for _ in range(8)]
+    cost = {}
+
+    graph[0] = [1, 3]
+    graph[3] = [1, 6, 4]
+    graph[1] = [6]
+    graph[4] = [2, 5]
+    graph[2] = [1]
+    graph[5] = [2, 6]
+    graph[6] = [4]
+
+    cost[(0, 1)] = 2
+    cost[(0, 3)] = 5
+    cost[(1, 6)] = 1
+    cost[(3, 1)] = 5
+    cost[(3, 6)] = 6
+    cost[(3, 4)] = 2
+    cost[(2, 1)] = 4
+    cost[(4, 2)] = 4
+    cost[(4, 5)] = 3
+    cost[(5, 2)] = 6
+    cost[(5, 6)] = 3
+    cost[(6, 4)] = 7
+
+    goal = [6]
+    answer = uniform_cost_search(goal, 0)
+    print("Minimum cost from 0 to 6 is =", answer[0])
