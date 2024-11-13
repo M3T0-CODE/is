dcc6a9f182204b63ce1952385397bd19f8cf27b7
fares <faresahmed02811@gmail.com>
2024-11-13 18:57:58 +0200

    greedy best first search

--- Main.py
+++ Main.py
@@ -1,46 +1,50 @@
-def greedy_best_first_search(graph, start, goal, heuristic):
-    frontier = [(start, heuristic[start])]
-    explored = set()
-    came_from = {start: None}
-
-    while frontier:
-        frontier.sort(key=lambda x: x[1])
-        current, _ = frontier.pop(0)
-
-        if current == goal:
-            break
-
-        explored.add(current)
-
-        for neighbor in graph[current]:
-            if neighbor not in explored and neighbor not in [node[0] for node in frontier]:
-                frontier.append((neighbor, heuristic[neighbor]))
-                came_from[neighbor] = current
-
-    path = []
-    while current is not None:
-        path.append(current)
-        current = came_from[current]
-    path.reverse()
-
-    return path
+from queue import PriorityQueue
 
 graph = {
-    'A': ['B', 'C'],
-    'B': ['D', 'E'],
-    'C': ['F'],
+    'A': [('B', 1), ('C', 3)],
+    'B': [('D', 4), ('E', 2)],
+    'C': [('F', 5), ('G', 1)],
     'D': [],
-    'E': ['F'],
-    'F': ['G'],
-    'G': []
+    'E': [('H', 1)],
+    'F': [],
+    'G': [('H', 2)],
+    'H': []
 }
 
 heuristic = {
-    'A': 7, 'B': 6, 'C': 3, 'D': 5,
-    'E': 2, 'F': 1, 'G': 0
+    'A': 6,
+    'B': 4,
+    'C': 3,
+    'D': 7,
+    'E': 2,
+    'F': 6,
+    'G': 1,
+    'H': 0
 }
 
-start = 'A'
-goal = 'G'
-path = greedy_best_first_search(graph, start, goal, heuristic)
-print("Path from start to goal:", path)
+def greedy_best_first_search(start, goal):
+    open_list = PriorityQueue()
+    open_list.put((heuristic[start], start))
+    came_from = {start: None}
+
+    while not open_list.empty():
+        _, current = open_list.get()
+        if current == goal:
+            path = []
+            while current:
+                path.append(current)
+                current = came_from[current]
+            path.reverse()
+            return path
+
+        for neighbor, _ in graph[current]:
+            if neighbor not in came_from:
+                came_from[neighbor] = current
+                open_list.put((heuristic[neighbor], neighbor))
+
+    return None
+
+start_node = 'A'
+goal_node = 'H'
+path = greedy_best_first_search(start_node, goal_node)
+print("Path from", start_node, "to", goal_node, ":", path)
